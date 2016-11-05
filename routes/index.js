142282e331cc56e08c1d35503e3b8a8ca487e892
hisatoshi <hisatoshi@toralab.org>
2016-11-05 10:12:52 +0900

    loginのそれを作った．データベース未定義のためエラーは起きる.ゆうしろうが頑張ってる

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/addNewUser', function(req, res) {
+  var newUser = new User(req.body);
+  newUser.save(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/')
+    }
+  });
+});
+
 module.exports = router;

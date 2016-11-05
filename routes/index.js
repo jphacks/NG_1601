f862c64e63ab92763272fe4a6cbd5a00bc835971
hisatoshi <hisatoshi@toralab.org>
2016-11-05 10:15:25 +0900

    消し忘れた文字を消した

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,10 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/addNewUser', function(req, res) {
-  var newUser = new User(req.body);
-  newUser.save(function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      res.redirect('/')
-    }
-  });
-});
-
-module.exports = router;
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+//
+//
+// module.exports = router;

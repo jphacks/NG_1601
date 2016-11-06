bbb454d824ee72679894aa886dbccf3f0fb31750
matsutani <yushiro.ma2ta2.21@gmail.com>
2016-11-06 10:13:53 +0900

    commit

--- routes/api.js
+++ routes/api.js
@@ -115,9 +115,11 @@ router.get('/get/:modelName', loginCheck, function(req,res){
 //↓の奴が出来る．
 
 // //user_idの女の子の情報を取得
-// router.get('/get/girl', loginCheck, function(req,res){
-//    statusCalcular(user_id);
-// });
+router.get('/get/girl', loginCheck, function(req,res){
+  console.log("ooooo");
+  console.log(statusCalculatar());
+   statusCalcular(user_id);
+});
 //
 //
 // //user_idで食べ物取得
